c5ab113f030b679fe67f6c796e795c58d0ae351f
Chrissy LeMaire <clemaire@gmail.com>
2020-09-27 13:48:43 +0200

    fix output

--- cache.ps1
+++ cache.ps1
@@ -1,24 +1,23 @@
 param (
-   [string]$Module, 
+   [string[]]$Module, 
    [switch]$List
 )
-$module = $Module.Split(",")
 $neededlist = @()
 $paths = @()
-foreach ($item in $module.Trim()) {
+$module = $Module.Split(",")
+
+foreach ($item in $module) {
    if ($List) {
       if (-not (Get-Module $item -ListAvailable)) {
          $neededlist += $item
       }
    } else {
       if (-not (Get-Module $item -ListAvailable)) {
-         # Write-Output "/usr/local/share/powershell/Modules/$item"
          $paths += "/home/runner/.local/share/powershell/Modules/$item"
       }
    }
 }
 if ($List) {
-   Write-Warning "LIST!"
    Write-Output "$($neededlist -join ', ')"
 } else {
    Write-Output "$($paths -join ', ')"
