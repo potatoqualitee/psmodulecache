b6045a5c7b860652c7c9d154ace81e41decdb4fe
Chrissy LeMaire <clemaire@gmail.com>
2020-09-27 13:13:11 +0200

    big ol change

--- cache.ps1
+++ cache.ps1
@@ -2,6 +2,7 @@ param ($Module)
 $module = $Module.Split(",")
 foreach ($item in $module.Trim()) {
    if (-not (Get-Module $item -ListAvailable)) {
-      Install-Module $item -Force
+      # Write-Output "/usr/local/share/powershell/Modules/$item"
+      Write-Output "/home/runner/.local/share/powershell/Modules/$item"
    }
 }
