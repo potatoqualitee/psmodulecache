a48f44cf2c327740916b5d032a59e70231c0f82d
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2020-09-27 16:30:32 +0200

    ready for prod (#3)

--- cache.ps1
+++ cache.ps1
@@ -1,7 +1,24 @@
-param ($Module)
-$module = $Module.Split(",")
-foreach ($item in $module.Trim()) {
-   if (-not (Get-Module $item -ListAvailable)) {
-      Install-Module $item -Force
+param (
+   [string[]]$Module,
+   [switch]$NeededOnly
+)
+$neededlist = @()
+$paths = @()
+foreach ($item in $module) {
+   if ($NeededOnly) {
+      if (-not (Get-Module $item -ListAvailable)) {
+         $neededlist += $item
+      }
    }
+   else {
+      if (-not (Get-Module $item -ListAvailable)) {
+         $paths += "/home/runner/.local/share/powershell/Modules/$item"
+      }
+   }
+}
+if ($NeededOnly) {
+   Write-Output "$($neededlist -join ', ')"
+}
+else {
+   Write-Output $($paths -join ', ')
 }
