9ed9393866a8b52708c1c3bd5f7bd42ddcb6cde2
Chrissy LeMaire <clemaire@gmail.com>
2020-09-27 13:28:25 +0200

    add a list hting

--- cache.ps1
+++ cache.ps1
@@ -1,8 +1,24 @@
-param ($Module)
+param (
+   [string]$Module, 
+   [switch]$List
+)
 $module = $Module.Split(",")
+$neededlist = @()
+$paths = @()
 foreach ($item in $module.Trim()) {
-   if (-not (Get-Module $item -ListAvailable)) {
-      # Write-Output "/usr/local/share/powershell/Modules/$item"
-      Write-Output "/home/runner/.local/share/powershell/Modules/$item"
+   if ($List) {
+      if (-not (Get-Module $item -ListAvailable)) {
+         $neededlist += $item
+      }
+   } else {
+      if (-not (Get-Module $item -ListAvailable)) {
+         # Write-Output "/usr/local/share/powershell/Modules/$item"
+         $paths += "/home/runner/.local/share/powershell/Modules/$item"
+      }
    }
 }
+if ($list) {
+   $neededlist -join ", "
+}
+# if no paths, then it shouldn't run the install-module anyway
+$paths -join ", "
