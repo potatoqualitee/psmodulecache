c66258c38dc900cb69c50d0a2b66efe43aee015d
Chrissy LeMaire <clemaire@gmail.com>
2020-09-28 23:20:51 +0200

    update with new routine

--- main.ps1
+++ main.ps1
@@ -17,6 +17,14 @@ switch ($Type) {
    'Needed' {
       Write-Output "$($neededlist -join ', ')"
    }
+   'ModulesToCache' {
+      $cachethis = @()
+      foreach ($module in $Modules) {
+         $item, $version = $module.Split(':')
+            $cachethis += $item
+      }
+      Write-Output "$($cachethis -join ', ')"
+   }
    'KeyGen' {
       if ($neededlist.count -gt 0) {
          Write-Output "$($PSVersionTable.Platform)-$($neededlist -join '-')"
