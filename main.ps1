6fab258b9e4c848fd120d9f067d22de16f795f5b
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2021-06-20 22:38:29 +0200

    update version (#11)

--- main.ps1
+++ main.ps1
@@ -1,14 +1,14 @@
 param (
    [string[]]$Module,
-   [ValidateSet("KeyGen","ModulePath")]
+   [ValidateSet("KeyGen","ModulePath", "SaveModule")]
    [string]$Type,
    [ValidateSet("pwsh","powershell")]
-   [string]$ShellToUse
+   [string]$Shell
 )
 
 switch ($Type) {
    'KeyGen' {
-      if ($ShellToUse -eq "powershell" -and $PSVersionTable.Platform -eq "Win32NT") {
+      if ($Shell -eq "powershell" -and $PSVersionTable.Platform -eq "Win32NT") {
          $versiontable = Invoke-Command -ScriptBlock { 
             powershell -command { $PSVersionTable } 
          }
@@ -20,13 +20,39 @@ switch ($Type) {
       } else {
          $platform = "Windows"
       }
-      Write-Output "$env:RUNNER_OS-$platform-$($versiontable.PSVersion)-$($Module -join '-')"
+      # all this splitting and joining accomodates for powershell and pwsh
+      Write-Output "v2-$env:RUNNER_OS-$platform-$($versiontable.PSVersion)-$(($Module.Split(",") -join '-').Replace(' ',''))"
    }
    'ModulePath' {
       if ($env:RUNNER_OS -eq "Windows") {
-         Write-Output ($env:PSModulePath.Split(";") | Select-Object -First 1)
+         $modpath = ($env:PSModulePath.Split(";") | Select-Object -First 1)
+         if ($Shell -eq "powershell") {
+            $modpath = $modpath.Replace("PowerShell","WindowsPowerShell")
+         }
+         Write-Output $modpath
       } else {
-         Write-Output ($env:PSModulePath.Split(":") | Select-Object -First 1)
+         ($env:PSModulePath.Split(":") | Select-Object -First 1)
+      }
+   }
+   'SaveModule' {
+      $moduleinfo = Import-CliXml -Path (Join-Path $home -ChildPath cache.xml)
+      Write-Output "Trusting PSGallery"
+      Set-PSRepository PSGallery -InstallationPolicy Trusted
+
+      $modulelist = $moduleinfo.Modules
+      Write-Output "Saving modules $modulelist to $($moduleinfo.ModulePath)"
+      $modules = $modulelist.Split(",").Trim()
+      $force = [bool]($moduleinfo.force)
+      $allowprerelease = [bool]($moduleinfo.allowprerelease)
+
+      foreach ($module in $modules) {
+         Write-Output "Installing module $module on PowerShell $($PSVersionTable.PSVersion)"
+         $item, $version = $module.Split(":")
+         if ($version) {
+            Save-Module $item -RequiredVersion $version -ErrorAction Stop -Force:$force -AllowPrerelease:$allowprerelease -Path $moduleinfo.ModulePath
+         } else {
+            Save-Module $item -ErrorAction Stop -Force:$force -AllowPrerelease:$allowprerelease -Path $moduleinfo.ModulePath
+         }
       }
    }
 }
